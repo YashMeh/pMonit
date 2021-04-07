30d7531934e0dc50b2f811a51bac7bf70b925c1c
YashMeh <mehrotra.yash9@gmail.com>
2021-04-08 00:00:26 +0530

    SC

--- main.go
+++ main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/struCoder/pidusage"
 )
 
 var ReadingsArray []*pidusage.SysInfo
+var Flag bool
 
-func doEvery(d time.Duration, f func(time.Time)) {
+func doEvery(d time.Duration, pID int, f func(_ time.Time, pID int)) {
 	for x := range time.Tick(d) {
-		f(x)
+		if !Flag {
+			f(x, pID)
+		} else {
+			fmt.Println("Pressed !!")
+			os.Exit(0)
+		}
 	}
 }
 
-func takeReadings(t time.Time) {
-	sysInfo, err := pidusage.GetStat(845)
+func takeReadings(t time.Time, pID int) {
+	sysInfo, err := pidusage.GetStat(pID)
 	if err != nil {
 		panic(err)
 	}
@@ -25,6 +34,28 @@ func takeReadings(t time.Time) {
 }
 
 func main() {
-	doEvery(500*time.Millisecond, takeReadings)
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			text, err := reader.ReadString('\n')
+			if err != nil {
+				return
+			}
+			text = strings.Replace(text, "\n", "", -1)
+
+			if strings.Compare("q", text) == 0 {
+				Flag = true
+			}
+		}
+
+	}()
+
+	// select {
+	// case msg := <-CH:
+	// 	fmt.Println("received message", msg)
+	// default:
+	// 	doEvery(5*time.Second, 845, takeReadings)
+	// }
+	doEvery(5*time.Second, 845, takeReadings)
 
 }
